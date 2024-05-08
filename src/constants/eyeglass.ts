34f75a7c62d1a654b838fc014a51846adae2d2c5
rashedrk <rashedrk001@gmail.com>
2024-05-08 16:56:46 +0600

    add eyeglass form

---
+++ src/constants/eyeglass.ts
@@ -0,0 +1,41 @@
+export const frameMaterial = ['Metal', 'Acetate', 'Plastic', 'Stainless-steel', 'Polycarbonate'];
+export const frameShapes = ['Rectangular', 'Round', 'Cat-Eye', 'Square', 'Wayfarer', 'Sports'];
+export const color = ['Black', 'Blue', 'Green', 'White', 'Gold', 'Gray', 'Silver'];
+export const frameType = ['Full-rim', 'Half-rim', 'Rimless'];
+export const frameSize = ['Narrow', 'Medium', 'Wide', 'Large'];
+export const lensType = ['Single-Vision', 'Progressive', 'Bifocal'];
+export const brands = ['Lenskart', 'John-Jacobs', 'Fossil', 'Ojos'];
+export const gender = ['Men', 'Women', 'Unisex'];
+
+export const frameMaterialOptions = frameMaterial.map(item => ({
+    label: item,
+    value: item.toLowerCase()
+}));
+export const frameShapesOptions = frameShapes.map(item => ({
+    label: item,
+    value: item.toLowerCase()
+}));
+export const colorOptions = color.map(item => ({
+    label: item,
+    value: item.toLowerCase()
+}));
+export const frameTypeOptions = frameType.map(item => ({
+    label: item,
+    value: item.toLowerCase()
+}));
+export const frameSizeOptions = frameSize.map(item => ({
+    label: item,
+    value: item.toLowerCase()
+}));
+export const lensTypeOptions = lensType.map(item => ({
+    label: item,
+    value: item.toLowerCase()
+}));
+export const brandsOptions = brands.map(item => ({
+    label: item,
+    value: item.toLowerCase()
+}));
+export const genderOptions = gender.map(item => ({
+    label: item,
+    value: item.toLowerCase()
+}));
--- src/pages/EyeglassManagement/AddEyeglass/AddEyeglass.tsx
+++ src/pages/EyeglassManagement/AddEyeglass/AddEyeglass.tsx
@@ -1,10 +1,33 @@
+import { Button, Col, Flex } from "antd";
+import CSForm from "../../../components/form/CSForm";
+import { FieldValues, SubmitHandler } from "react-hook-form";
+import CSInput from "../../../components/form/CSInput";
+import CSSelect from "../../../components/form/CSSelect";
+import { brandsOptions, frameMaterialOptions, frameShapesOptions, frameSizeOptions, frameTypeOptions, genderOptions, lensTypeOptions } from "../../../constants/eyeglass";
 
 const AddEyeglass = () => {
-    return (
-        <div>
-            This is add new eye glass
-        </div>
-    );
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    console.log(data);
+  };
+  return (
+    <Flex justify="center" align="center">
+      <Col span={6}>
+        <CSForm onSubmit={onSubmit}>
+          <CSInput type="text" label="Name" name="name"/>
+          <CSInput type="number" label="Quantity" name="quantity"/>
+          <CSInput type="number" label="Price" name="price"/>
+          <CSSelect label="Frame Material" name="frameMaterial" options={frameMaterialOptions}/>
+          <CSSelect label="Frame Shapes" name="frameShapes" options={frameShapesOptions}/>
+          <CSSelect label="Frame Type" name="frameType" options={frameTypeOptions}/>
+          <CSSelect label="Frame Size" name="frameSize" options={frameSizeOptions}/>
+          <CSSelect label="Lens Type" name="lensType" options={lensTypeOptions}/>
+          <CSSelect label="Brand" name="brand" options={brandsOptions}/>
+          <CSSelect label="Gender" name="gender" options={genderOptions}/>
+          <Button htmlType="submit">Submit</Button>
+        </CSForm>
+      </Col>
+    </Flex>
+  );
 };
 
 export default AddEyeglass;

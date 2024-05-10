72deda0806e0267c371c44f5ee96d12715a15e6c
rashedrk <rashedrk001@gmail.com>
2024-05-10 23:45:48 +0600

    search option added to sell

--- src/pages/Sell/Sell.tsx
+++ src/pages/Sell/Sell.tsx
@@ -1,21 +1,37 @@
-import { Col,  Row, Spin } from "antd";
+import { Col, Row, Spin } from "antd";
 import { useGetAllEyeglassesQuery } from "../../redux/features/eyeGlass/eyeglassApi";
 import EyeglassCard from "../../components/ui/Cards/EyeglassCard";
+import Search from "antd/es/input/Search";
+import { useState } from "react";
+import { TQueryParams } from "../../types/global.type";
 
 const Sell = () => {
-  const { data: eyeglassData, isFetching } =
-    useGetAllEyeglassesQuery(undefined);
+  const [params, setParams] = useState<TQueryParams[] | undefined>(undefined);
+  const { data: eyeglassData, isFetching } = useGetAllEyeglassesQuery(params);
+
+  const onSearch = (value: string) => {
+    setParams([{ name: "searchTerm", value }]);
+  };
 
   return (
     <>
+      <Search
+        placeholder="input search text"
+        onSearch={onSearch}
+        style={{ width: 300, marginBottom: 20 }}
+        size="large"
+        allowClear
+      />
       {isFetching ? (
-        <Spin tip="Loading" size="large"></Spin>
+        <Spin fullscreen tip="Loading" size="large"></Spin>
       ) : (
         <Row gutter={8}>
           {eyeglassData?.data?.map((eyeglass) => (
-            <Col span={6}><EyeglassCard key={eyeglass._id} eyeglass={eyeglass} /></Col>
+            <Col span={6}>
+              <EyeglassCard key={eyeglass._id} eyeglass={eyeglass} />
+            </Col>
           ))}
-          </Row>
+        </Row>
       )}
     </>
   );

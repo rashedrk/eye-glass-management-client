d57c49c80035bb46fd54824e19102ad604f70fac
rashedrk <rashedrk001@gmail.com>
2024-05-13 06:17:10 +0600

    jwt error issue solve

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -1,5 +1,9 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { BaseQueryApi, BaseQueryFn, DefinitionType, FetchArgs, createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { RootState } from "../store";
+import { toast } from "sonner";
+import { logout } from "../features/auth/authSlice";
+import { TResponse } from "../../types/global.type";
 
 const baseQuery = fetchBaseQuery({
     baseUrl: 'https://eye-glass-management-v2-server.vercel.app/api/v1',
@@ -13,9 +17,26 @@ const baseQuery = fetchBaseQuery({
     }
 });
 
+const baseQueryWithErrorCheck: BaseQueryFn<FetchArgs, BaseQueryApi, DefinitionType> = async (args, api, extraOptions): Promise<any> => {
+    const result = await baseQuery(args, api, extraOptions) as TResponse<any>;
+
+    if (result?.error?.status === 404) {
+        toast.error(result.error.data.message);
+    }
+    if (result?.error?.status === 403) {
+        toast.error(result.error.data.message);
+    }
+
+    if (result?.error?.status === 401 || result?.error?.data.message === "jwt expired") {
+        api.dispatch(logout());
+    }
+
+    return result;
+}
+
 export const baseApi = createApi({
     reducerPath: 'baseApi',
     tagTypes: ['eyeglass'],
-    baseQuery,
+    baseQuery:baseQueryWithErrorCheck,
     endpoints: () => ({})
 });
